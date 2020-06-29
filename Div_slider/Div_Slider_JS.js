6b7e51fb43adf35e9ab7041e5e3df8aa5e1390d5
rohitps3282 <rohitps3282@gmail.com>
2020-06-29 22:04:43 +0530

    JS

--- Div_slider/Div_Slider_JS.js
+++ Div_slider/Div_Slider_JS.js
@@ -15,7 +15,7 @@ var slideIndex = 0;
       }
       slides[slideIndex-1].style.display = "block";  
       dots[slideIndex-1].className += " active";
-      setTimeout(showSlides, 10000); // Change image every 2 seconds
+      setTimeout(showSlides, 2000); // Change image every 2 seconds
     }
 
     
